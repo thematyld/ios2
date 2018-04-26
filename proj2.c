12da7a7cd896ad7e5d8f0947e8a15e1409e68133
Martin Bartos <thematyld@gmail.com>
2018-04-26 18:11:26 +0200

    Begin synchronization

--- proj2.c
+++ proj2.c
@@ -10,9 +10,13 @@
 
 
 #define COUNT_PARAM 5
+#define SHM_NAME "/xbarto96_shm_name"
 #define SEM1_NAME "/xbarto96_sem1"
 #define SEM2_NAME "/xbarto96_sem2"
+
+
 #define LOCKED 0
+
 
 //--------------TypeDef-----------------//
 typedef enum{false=0,true=1} boolean;
@@ -25,62 +29,78 @@ typedef struct{
 
 //---------------Global--Variables----------//
 
-Params param;
-sem_t *sem1=NULL;
-sem_t *sem2=NULL;
+Params Tparam;
+sem_t *bus=NULL;
+sem_t *tmpRider=NULL;
+sem_t **rider;
+int sharedId=0;
+int *counter=NULL;
+
 
 //---------------Prototype--functions--------//
-void errorArgs();
-void initArgs(int,char**,int);
+void error(char* text);
+void initArgs(int argc,char** args,int count);
 
 
 //----------Main-----------------------//
 int main(int argc,char** args) {
     setbuf(stdout,NULL);
     initArgs(argc,args,COUNT_PARAM);
-
-    if((sem1=sem_open(SEM1_NAME, O_CREAT | O_EXCL,0666,LOCKED))==SEM_FAILED){
-
-    }
-
-    if((sem2=sem_open(SEM2_NAME, O_CREAT | O_EXCL,0666,LOCKED))==SEM_FAILED){
+    if((bus=sem_open(SEM1_NAME,O_CREAT|O_EXCL,0666,LOCKED))==SEM_FAILED){
 
     }
+    if((tmpRider=sem_open(SEM2_NAME,O_CREAT|O_EXCL,0666,LOCKED))==SEM_FAILED){
 
-    pid_t processPid=fork();
+    }
+    sharedId=shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    counter=shmat(sharedId, NULL, 0);
 
-    if(processPid==0) {
-        for(int i=0;i<5;i++) {
-            sem_wait(sem1);
-            printf("CHILD %d\n",i);
-            sem_post(sem2);
+    pid_t* riders= malloc(Tparam.R* sizeof(pid_t));
+    pid_t processPid;
+    if(riders==NULL)
+        error("Error - Create array of pid_t riders!!");
+
+    for(unsigned int i=1;i<=Tparam.R;i++){
+        processPid=fork();
+
+        if(processPid==0){
+
+        } else if(processPid>0){
+            riders[i]=processPid;
+        } else{
+            error("Error - Create riders!!");
         }
-        //sem_wait(sem1);
-    }
-    else if(processPid>0) {
-        for(int i=0;i<5;i++){
-            sem_post(sem1);
-            printf("Parents %d\n",i);
-            sem_wait(sem2);
-        }
+
     }
 
-    printf("End child \n");
-    sem_post(sem1);
+    for(int i=1;i<=Tparam.R;i++){
+        printf("%d\n",riders[i]);
+    }
+
 
     waitpid(processPid,NULL,0);
     printf("End paren \n");
 
-    sem_close(sem1);
-    sem_close(sem2);
+    sem_close(bus);
+    sem_close(tmpRider);
 
     sem_unlink(SEM1_NAME);
     sem_unlink(SEM2_NAME);
+
     return 0;
 }
 
-void errorArgs(){
-    fprintf(stderr,"Syntax Error!!\n");
+void createRider(){
+
+}
+
+void createBus(){
+
+}
+
+
+void error(char* text){
+    fprintf(stderr,"%s\n",text);
     exit(1);
 }
 
@@ -89,35 +109,34 @@ void initArgs(int argc,char** args,int count){
         for(int i=1;i<count;i++){
             int tmp=(int)strtol(args[i],NULL,10);
             if(tmp<0)
-                errorArgs();
+                error("Bad arguments!!");
             switch(i){
                 case 1:
                     if(tmp>0)
-                        param.R=(unsigned int)tmp;
+                        Tparam.R=(unsigned int)tmp;
                     else
-                        errorArgs();
+                        error("Bad arguments!!");
                     break;
                 case 2:
                     if(tmp>0)
-                        param.C=(unsigned int)tmp;
+                        Tparam.C=(unsigned int)tmp;
                     else
-                        errorArgs();
+                        error("Bad arguments!!");
                     break;
                 case 3:
                     if(tmp>=0 && tmp<=1000)
-                        param.ART=(unsigned int)tmp;
+                        Tparam.ART=(unsigned int)tmp;
                     else
-                        errorArgs();
+                        error("Bad arguments!!");
                     break;
                 case 4:
                     if(tmp>=0 && tmp<=1000)
-                        param.ABT=(unsigned int)tmp;
+                        Tparam.ABT=(unsigned int)tmp;
                     else
-                        errorArgs();
+                        error("Bad arguments!!");
                     break;
             }
         }
     }
     else
-        errorArgs();
-}
+        error("Bad arguments!!");}
